dcefeea5c80476211f34ae2967cea7d5859db794
Panatda04 <6514120004@mut.ac.th>
2023-08-21 18:51:04 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    public function showLogin(){
+        return view('content.login');
+    }
+    public function checkLogin(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required','email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/content');
+        }
+        return back()->withErrors([
+            'email' => 'Creadentals do not match our records'
+        ]);
+    }
+}
+
+
